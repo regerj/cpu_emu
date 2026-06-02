5b78477396eb54cbbdb21f674fc55adf73c08f00
Jacob Reger <regerj@protonmail.com>
2026-06-02 01:07:58 -0700

    add initial cache support

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use bitfield_struct::bitfield;
+
 use crate::{
     WORD,
     mem::MemoryController,
@@ -11,6 +13,7 @@ use crate::{
 pub struct Cpu {
     regs: Vec<WORD>,
     mc: MemoryController,
+    cache: Cache,
 }
 
 impl Cpu {
@@ -18,6 +21,7 @@ impl Cpu {
         Self {
             regs: vec![0; 2],
             mc,
+            cache: Cache::new(),
         }
     }
 
@@ -32,7 +36,7 @@ impl Cpu {
                 };
 
                 let src_word = if src.deref {
-                    self.mc.read(src_word)
+                    self.read_addr(src_word)
                 } else {
                     src_word
                 };
@@ -43,7 +47,7 @@ impl Cpu {
                     }
                     // We asserted storable, so we know the literal is an address
                     OperandInner::Literal => {
-                        let dest_val = self.mc.read(dest.word);
+                        let dest_val = self.read_addr(dest.word);
                         self.mc.write(dest.word, dest_val + src_word);
                     }
                 }
@@ -57,7 +61,7 @@ impl Cpu {
                 };
 
                 let src_word = if src.deref {
-                    self.mc.read(src_word)
+                    self.read_addr(src_word)
                 } else {
                     src_word
                 };
@@ -68,7 +72,7 @@ impl Cpu {
                     }
                     // We asserted storable, so we know the literal is an address
                     OperandInner::Literal => {
-                        let dest_val = self.mc.read(dest.word);
+                        let dest_val = self.read_addr(dest.word);
                         self.mc.write(dest.word, dest_val - src_word);
                     }
                 }
@@ -82,7 +86,7 @@ impl Cpu {
                 };
 
                 let src_word = if src.deref {
-                    self.mc.read(src_word)
+                    self.read_addr(src_word)
                 } else {
                     src_word
                 };
@@ -99,10 +103,65 @@ impl Cpu {
             }
         }
     }
+
+    /// Attempt to read the given address from the cache. Failing that, fallback to reading it from
+    /// the memory controller.
+    fn read_addr(&self, addr: WORD) -> WORD {
+        let cache_addr = CacheAddr::from_bits(addr);
+        if let Some(cache_entry) = self.cache.lookup(&cache_addr) {
+            let val = (cache_entry >> (8 * cache_addr.offset()) & 0xFF) as u8;
+            return val;
+        }
+
+        self.mc.read(addr)
+    }
 }
 
 impl Drop for Cpu {
     fn drop(&mut self) {
         self.mc.kill();
     }
+}
+
+/// The cache is a 1 level 2-way set associative cache.
+///
+/// It utilizes 2 byte cache line size and has a total capacity of 16 cache lines.
+#[derive(Debug)]
+struct Cache {
+    inner: [[Option<CacheEntry>; 2]; 8],
+}
+
+impl Cache {
+    fn new() -> Self {
+        Self {
+            inner: [[None; 2]; 8],
+        }
+    }
+
+    fn lookup(&self, addr: &CacheAddr) -> Option<u16> {
+        self.inner[addr.index()].iter().find_map(|entry| {
+            let entry = (*entry)?;
+            if entry.tag == addr.tag() {
+                Some(entry.val)
+            } else {
+                None
+            }
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct CacheEntry {
+    tag: u8,
+    val: u16,
+}
+
+#[bitfield(u8)]
+struct CacheAddr {
+    #[bits(1)]
+    offset: usize,
+    #[bits(3)]
+    index: usize,
+    #[bits(4)]
+    tag: u8,
 }
